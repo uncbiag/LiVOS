8c26cb03f570942a0c9a26f5bf7a8d378fc9ac60
Qin Liu <qinliu2020@gmail.com>
2025-08-31 21:58:03 -0700

    Update download.py

--- download.py
+++ download.py
@@ -6,28 +6,28 @@ save_folder ='weights'
 os.makedirs(save_folder, exist_ok=True)
 
 # weights for livos-nomose-480p
-weights_url = 'https://drive.google.com/uc?id=1tG_BxCTWp_o9YH0vBqZqLC9KBsEGSsaH'
+weights_url = 'https://drive.google.com/uc?id=1ZjhVMUppZ5OZipCALU9ZYwiVF5DEPBxX'
 output = f'{save_folder}/livos-nomose-480p.pth'
 gdown.download(weights_url, output, quiet=False)
 
 # weights for livos-nomose-ft-480p
-weights_url = 'https://drive.google.com/uc?id=1ToIDo6PIYF7lQGfO4F7HuHneyatKGWnx'
+weights_url = 'https://drive.google.com/uc?id=1rUxSjKreh5Q_X7FHvFRyoKBR7GqJr04P'
 output = f'{save_folder}/livos-nomose-ft-480p.pth'
 gdown.download(weights_url, output, quiet=False)
 
 # weights for livos-wmose-480p
-weights_url = 'https://drive.google.com/uc?id=13FVuxcEwNRfY70PA3O9pOyPO7Gx7Zl5N'
+weights_url = 'https://drive.google.com/uc?id=1lhfnRCe1yPfB9dcWSA6hv7ABPzHFuNCY'
 output = f'{save_folder}/livos-wmose-480p.pth'
 gdown.download(weights_url, output, quiet=False)
 
 
 # uncomment below to download 1024p/2048p trained models.
 # # weights for livos-nomose-1024p
-# weights_url = 'https://drive.google.com/uc?id=1ZBY2IOOZOy8bBDUdblRiHvN6x7zWY4JU'
+# weights_url = 'https://drive.google.com/uc?id=1_NjVaumXO8IPsFocXdRAvs6hqd-TOSGH'
 # output = f'{save_folder}/livos-nomose-1024p.pth'
 # gdown.download(weights_url, output, quiet=False)
 
 # # weights for livos-nomose-2048p
-# weights_url = 'https://drive.google.com/uc?id=1ebAf0d7T9yRfm03pt96JqdxBadb27zV5'
+# weights_url = 'https://drive.google.com/uc?id=1Od-djH3AUDEXJmXmqygCUSrFWUmVyNBG'
 # output = f'{save_folder}/livos-nomose-2048p.pth'
 # gdown.download(weights_url, output, quiet=False)
